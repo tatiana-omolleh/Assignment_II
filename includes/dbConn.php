27461f857281afc86419d79ba1a67bb318a7ca58
tatiana-omolleh <tatianaomolleh12@gmail.com>
2023-09-29 13:39:17 +0300

    Uploads

---
+++ includes/dbConn.php
@@ -0,0 +1,54 @@
+<?php
+class dbConn {
+    private $connection;
+    
+    // Define the database connection variables here
+    private $db_host = 'localhost';
+    private $db_name = 'info';
+    private $db_user = 'root';
+    private $db_pass = '';
+    private $db_port = '3306';
+
+    public function __construct() {
+        $this->connection();
+    }
+
+    private function connection() {
+        $dsn = "mysql:host=$this->db_host;port=$this->db_port;dbname=$this->db_name";
+        
+        try {
+            $this->connection = new PDO($dsn, $this->db_user, $this->db_pass);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public function insert($table, $data){
+        ksort($data);
+        $fieldNames = implode('`, `', array_keys($data));
+        $fieldPlaceholders = ':' . implode(', :', array_keys($data));
+        
+        $sql = "INSERT INTO $table (`$fieldNames`) VALUES ($fieldPlaceholders)";
+        $stmt = $this->connection->prepare($sql);
+    
+        // Bind values to placeholders
+        foreach ($data as $key => $value) {
+            $stmt->bindValue(":$key", $value);
+        }
+    
+        try {
+            $stmt->execute();
+            return true; // Insertion was successful
+        } catch (PDOException $e) {
+            return false; // Insertion failed
+        }
+    }
+    
+    public function execute($sql){
+        $result = $this->connection->prepare($sql);
+        $result->execute();
+        return $result->fetchAll(PDO::FETCH_ASSOC)[0];
+    }
+}
+?>
---
+++ sql/info.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 29, 2023 at 12:38 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `info`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `email_address` varchar(50) NOT NULL DEFAULT '',
+  `contact` varchar(60) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`name`, `email_address`, `contact`) VALUES
+('0700111099', '', ''),
+('Tatiana Omolleh', 'tatianaomolleh12@gmail.com', '0700111099');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`contact`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
